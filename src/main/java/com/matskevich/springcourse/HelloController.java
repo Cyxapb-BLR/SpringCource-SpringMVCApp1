3b4f2ce9be36e1d4dff8bb3cd1957e584f494f06
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-06 15:44:46 -0500

    L17 - Configuration by Java code, NO xml(deleted) - web.xml->MySpringMvcDispatcherServletInitializer, applicationContextMVC.xml->SpringConfig, added Java Servlet Api dependency

--- src/main/java/com/matskevich/springcourse/HelloController.java
+++ src/main/java/com/matskevich/springcourse/HelloController.java
@@ -1,4 +1,4 @@
-package com.matskevich.springcource;
+package com.matskevich.springcourse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
