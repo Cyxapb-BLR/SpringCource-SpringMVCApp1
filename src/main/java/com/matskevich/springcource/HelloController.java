2ff8a5618b793d28cc9d8fa27901aa1d89f997a9
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-06 14:03:37 -0500

    L16 - edited, created HelloController and hello_world.html in views. checking http://localhost:8080/hello-world

---
+++ src/main/java/com/matskevich/springcource/HelloController.java
@@ -0,0 +1,12 @@
+package com.matskevich.springcource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+class HelloController {
+    @GetMapping("hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
