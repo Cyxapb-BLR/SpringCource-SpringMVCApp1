a8d5402da34121fbf2536af7b7661720e75ed671
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-09 15:40:42 -0500

    L21 - Model - added Model, model.addAttribute(), added request result in View with ThymeLeaf

--- src/main/java/com/matskevich/springcourse/controllers/FirstController.java
+++ src/main/java/com/matskevich/springcourse/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.matskevich.springcourse.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,8 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 public class FirstController {
     @GetMapping("hello")
     public String helloPage(@RequestParam(value = "firstname", required = false) String firstName,
-                            @RequestParam(value = "lastname", required = false) String lastName) {
-        System.out.println("Hello, " + firstName + " " + lastName);
+                            @RequestParam(value = "lastname", required = false) String lastName, Model model) {
+        //System.out.println("Hello, " + firstName + " " + lastName);
+        model.addAttribute("message", "Hello, " + firstName + " " + lastName);
         return "first/hello";
     }
 
