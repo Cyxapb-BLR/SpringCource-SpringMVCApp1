4740000bb9384f19f5bfab6da00a070f492e6766
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-09 15:09:31 -0500

    L20 - GET request -  HttpServletRequest->@RequestParam, checking http://localhost:8080/first/hello?firstname=Vadim&lastname=Matskevich, error 400 without parameters for http://localhost:8080/first/hello

--- src/main/java/com/matskevich/springcourse/controllers/FirstController.java
+++ src/main/java/com/matskevich/springcourse/controllers/FirstController.java
@@ -3,6 +3,7 @@ package com.matskevich.springcourse.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -10,9 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("first")
 public class FirstController {
     @GetMapping("hello")
-    public String helloPage(HttpServletRequest request) {
-        String firstName = request.getParameter("firstname");
-        String lastName = request.getParameter("lastname");
+    public String helloPage(@RequestParam("firstname") String firstName,
+                            @RequestParam("lastname") String lastName) {
         System.out.println("Hello, " + firstName + " " + lastName);
         return "first/hello";
     }
