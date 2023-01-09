0ae0669d0df40955843e1a6d4bbc82a5e46eadb0
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-09 14:59:39 -0500

    L20 - GET request - added HttpServletRequest in controller method, request.getParameter(), checking at http://localhost:8080/first/hello?firstname=vadim&lastname=matskevich

--- src/main/java/com/matskevich/springcourse/controllers/FirstController.java
+++ src/main/java/com/matskevich/springcourse/controllers/FirstController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("first")
 public class FirstController {
     @GetMapping("hello")
-    public String helloPage() {
+    public String helloPage(HttpServletRequest request) {
+        String firstName = request.getParameter("firstname");
+        String lastName = request.getParameter("lastname");
+        System.out.println("Hello, " + firstName + " " + lastName);
         return "first/hello";
     }
 
