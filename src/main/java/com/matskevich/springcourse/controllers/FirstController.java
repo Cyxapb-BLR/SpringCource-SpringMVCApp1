37351a834bf87686925f603f45f00f19dc9fcf63
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-09 18:30:23 -0500

    HW21 - Calculator - created calculator.html in view, added calculate() in controller, buttons for view

--- src/main/java/com/matskevich/springcourse/controllers/FirstController.java
+++ src/main/java/com/matskevich/springcourse/controllers/FirstController.java
@@ -1,12 +1,11 @@
 package com.matskevich.springcourse.controllers;
 
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("first")
@@ -22,5 +21,23 @@ public class FirstController {
     @GetMapping("goodbye")
     public String goodbyePage() {
         return "first/goodbye";
+    }
+
+    @GetMapping("calculator")
+    public String calculate(@RequestParam(value = "a", required = false) Integer a,
+                            @RequestParam(value = "b", required = false) Integer b,
+                            @RequestParam(value = "action", required = false) String action, Model model) {
+        if (a != null && b != null && action != null) {
+            if (action.equals("addition")) {
+                model.addAttribute("result", a + b);
+                System.out.println(a + b);
+            } else if (action.equals("subtraction"))
+                model.addAttribute("result", a - b);
+            else if (action.equals("multiplication"))
+                model.addAttribute("result", a * b);
+            else if (action.equals("division") & b != 0)
+                model.addAttribute("result", a / b);
+        }
+        return "first/calculator";
     }
 }
