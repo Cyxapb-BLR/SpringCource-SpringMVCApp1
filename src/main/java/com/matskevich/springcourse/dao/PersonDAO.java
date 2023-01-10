9ec5de2c51f9ac29673d947fbf51a6a15133f20f
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-10 13:08:29 -0500

    L22 - CRUD, REST, DAO - created Person and PersonDAO

---
+++ src/main/java/com/matskevich/springcourse/dao/PersonDAO.java
@@ -0,0 +1,27 @@
+package com.matskevich.springcourse.dao;
+
+import com.matskevich.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Vadim"));
+        people.add(new Person(++PEOPLE_COUNT, "Кацянятка"));
+        people.add(new Person(++PEOPLE_COUNT, "Slayer"));
+        people.add(new Person(++PEOPLE_COUNT, "Vera"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
