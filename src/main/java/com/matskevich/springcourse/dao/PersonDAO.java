a85ba0c559d62a2bb1e78941a7efddf5fa6efa94
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-11 18:32:14 -0500

    L26 - JDBC API - added jdbc-driver postgresql in pom.xml, List<Person> -> table Person in first_db, update index()=>get people from table of db

--- src/main/java/com/matskevich/springcourse/dao/PersonDAO.java
+++ src/main/java/com/matskevich/springcourse/dao/PersonDAO.java
@@ -3,43 +3,73 @@ package com.matskevich.springcourse.dao;
 import com.matskevich.springcourse.models.Person;
 import org.springframework.stereotype.Component;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
+    private static final String URL = "jdbc:postgresql://localhost:5432/first_db";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "postgres";
 
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Vadim", 34, "vadim@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Кацянятка", 26, "keterina@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Slayer", 32, "slayer@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Vera", 33, "vera@gmail.com"));
+    private static Connection connection;
+
+    static { // create connection to postgresql
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public List<Person> index() {
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();     //contains sql-request
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);  // get data from table
+
+            while (resultSet.next()) {      // check every lane from table
+                Person person = new Person();
+
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setAge(resultSet.getInt("age"));
+                person.setEmail(resultSet.getString("email"));
+
+                people.add(person);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        //return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+        return null;
     }
 
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+        /*person.setId(++PEOPLE_COUNT);
+        people.add(person);*/
     }
 
     public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
+       /* Person personToBeUpdated = show(id);
         personToBeUpdated.setName(updatedPerson.getName());
         personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
+        personToBeUpdated.setEmail(updatedPerson.getEmail());*/
     }
 
     public void delete(int id) {
-        people.removeIf(p -> p.getId() == id);
+       // people.removeIf(p -> p.getId() == id);
     }
 }
