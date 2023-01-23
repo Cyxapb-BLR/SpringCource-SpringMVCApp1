72ec04389c3b60cbcfd8c73ef1ee369e09ca6fc6
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-23 13:55:45 -0500

    Updated Person, dao and views

--- src/main/java/com/matskevich/springcourse/models/Person.java
+++ src/main/java/com/matskevich/springcourse/models/Person.java
@@ -1,6 +1,5 @@
 package com.matskevich.springcourse.models;
 
-import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
@@ -10,20 +9,15 @@ public class Person {
     @NotEmpty(message = "Name should not be empty")
     @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
     private String name;
-    @Min(value = 0, message = "Age should be greater than 0")
-    private int age;
-    @NotEmpty(message = "Email should not be empty")
-    @Email
-    private String email;
+    @Min(value = 1900, message = "Year should be greater than 1900")
+    private int yearOfBirth;
 
     public Person() {
     }
 
-    public Person(int id, String name, int age, String email) {
-        this.id = id;
+    public Person(int id, String name, int yearOfBirth, String email) {
         this.name = name;
-        this.age = age;
-        this.email = email;
+        this.yearOfBirth = yearOfBirth;
     }
 
     public int getId() {
@@ -42,19 +36,11 @@ public class Person {
         this.name = name;
     }
 
-    public int getAge() {
-        return age;
+    public int getYearOfBirth() {
+        return yearOfBirth;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
     }
 }
