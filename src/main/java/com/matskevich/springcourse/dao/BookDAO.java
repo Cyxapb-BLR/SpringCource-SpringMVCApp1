8ae8b08c72e4673e4fcfdbed427b54caccf00ac9
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-01-23 16:01:40 -0500

    CRUD for BookDAO

---
+++ src/main/java/com/matskevich/springcourse/dao/BookDAO.java
@@ -0,0 +1,41 @@
+package com.matskevich.springcourse.dao;
+
+import com.matskevich.springcourse.models.Book;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class BookDAO {
+    private final JdbcTemplate jdbcTemplate;
+
+    public BookDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Book> index() {
+        return jdbcTemplate.query("SELECT * FROM Book", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Book show(int id) {
+        return jdbcTemplate.query("SELECT * FROM Book WHERE id=?",
+                        new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
+                .stream().findAny().orElse(null);
+    }
+
+    public void save(Book book) {
+        jdbcTemplate.update("INSERT INTO Book(title, author, year) values (?,?,?)",
+                book.getTitle(), book.getAuthor(), book.getYear());
+    }
+
+    public void update(int id, Book updatedBook) {
+        jdbcTemplate.update("UPDATE Book SET title=?,author=?,year=? WHERE id=?",
+                updatedBook.getTitle(), updatedBook.getAuthor(), updatedBook.getYear(), id);
+    }
+
+    public void delete(int id) {
+        jdbcTemplate.update("DELETE FROM Book WHERE id=?", id);
+    }
+}
