d0309b29b34c3dfb710ab8bfb91da4c42d62dbc5
Cyxapb-BLR <cyxapb-511@yandex.ru>
2023-02-03 18:04:39 -0500

    add DAO for special query to DB (by SQL)

---
+++ src/main/java/com/matskevich/springcourse/dao/BookDAO.java
@@ -0,0 +1,8 @@
+package com.matskevich.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BookDAO {
+    //  for special query to DB (by SQL)
+}
---
+++ src/main/java/com/matskevich/springcourse/dao/PersonDAO.java
@@ -0,0 +1,8 @@
+package com.matskevich.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PersonDAO {
+    //  for special query to DB (by SQL)
+}
